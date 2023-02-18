1260c366c790e06933f159c009b782973f179f64
Debapriyo Das <115923073+debapriyo007@users.noreply.github.com>
2023-02-18 20:45:12 +0530

    new code added

---
+++ 10_Array_List/arrlist16.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class arrlist16 {
+
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+        int bpoint = -1;
+        int n = list.size();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                bpoint = i;
+                break;
+            }
+        }
+
+        int lp = bpoint + 1;
+        int rp = bpoint;
+        while (lp != rp) {
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % n;
+            } else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int target = 10006;
+
+        System.out.println(pairSum2(list, target));
+
+    }
+}

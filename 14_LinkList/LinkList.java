973eb7116be837ad93a47c7afe1ffa122991e8ce
Debapriyo Das <debapriyo00@gmail.com>
2023-04-13 12:55:17 +0530

    Add more opertaion..

--- 14_LinkList/LinkList.java
+++ 14_LinkList/LinkList.java
@@ -17,12 +17,17 @@ public class LinkList {
     public static Node head;
     public static Node tail;
 
-    // Creating methords..
+    // define size..
+    public static int size;
+
+    // ----------------------Creating methords for Add
+    // linklist--------------------------------------------..
 
     // create "addFirst" fun..
     public void addFirst(int data) {
         // step 1: create a new node...
         Node newNode = new Node(data);
+        size++;
 
         // if the linklist is empty..then head and tail both are same..
         if (head == null) {
@@ -41,6 +46,7 @@ public class LinkList {
 
         // step 1: create a new node...
         Node newNode = new Node(data);
+        size++;
 
         // base case..
         if (head == null) {
@@ -59,13 +65,14 @@ public class LinkList {
     // add middle in the linklist..
 
     public void addMiddle(int indx, int data) {
-        //some one add at index 0 ..
-        if(indx ==0){
+        // some one add at index 0 ..
+        if (indx == 0) {
             addFirst(data);
             return;
         }
         // create new node...
         Node newNode = new Node(data);
+        size++;
         Node tem = head;
         int i = 0; // intial the index from 0
         while (i < indx - 1) {
@@ -80,7 +87,111 @@ public class LinkList {
 
     }
 
-    // Print Linklist function...
+    // ---------------------Remove
+    // Functions------------------------------------------------------------------
+
+    // Remove first..
+    public int removeFist() {
+        // special cases...
+        if (size == 0) {
+            System.out.println("LinkList is empty!");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    // remove last..
+    public int removeLast() {
+        // corner cases..
+        if (size == 0) {
+            System.out.println("The Linklist is empty!");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+
+    }
+
+    // -------------------Search function-------------------------
+
+    public int iterativeSearch(int key) { // Liner Time complexity..0(n)
+        // initial the index...
+        int indx = 0;
+        // store the tem in head..
+        Node tem = head;
+        // check the key is present or not in the linklist..
+
+        while (tem != null) {
+            if (tem.data == key) {
+                return indx;
+            }
+            // tem will be pointing next node..
+            tem = tem.next;
+            // index will updated...
+            indx++;
+        }
+        return -1;
+    }
+
+    // helper function....
+
+    public int helper(Node head, int key) {
+
+        // base case..
+        if (head == null) {
+            return -1;
+        }
+
+        if (head.data == key) {
+            return 0;
+        }
+        int indx = helper(head.next, key);
+        if (indx == -1) {
+            return -1;
+        }
+        return indx + 1;
+    }
+
+    public int recursiveSearch(int key) {
+        return helper(head, key);
+    }
+
+    // .................Reverse Linklist.....................................
+
+    public void Reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next; // Initilize the next in the loop..
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    // ----------------------------------Print Linklist----------------------
 
     public void printLinklist() {
         if (head == null) {
@@ -104,13 +215,35 @@ public class LinkList {
         // inside the class. like ---> add(), remove(), printll(), search().
         // ll.head = new Node(1);
         // ll.head.next = new Node(1);
-        //ll.printLinklist();
-        ll.addFirst(2);
-        ll.addFirst(1);
+        // ll.printLinklist();
+        // ll.addFirst(2);
+        // ll.addFirst(1);
+        // ll.addLast(3);
+        // ll.addLast(4);
+        // ll.addMiddle(2, 9);
+        // ll.printLinklist();
+        // ll.removeFist();
+        // ll.printLinklist();
+        // ll.removeLast();
+
+        ll.addLast(1);
+        ll.addLast(2);
         ll.addLast(3);
         ll.addLast(4);
-        ll.addMiddle(2, 9);
+        ll.addLast(5);
+
         ll.printLinklist();
+        ll.Reverse();
+        ll.printLinklist();
+
+        int index = ll.recursiveSearch(4);
+        if (index == -1) {
+            System.out.println("The key is not found!!");
+        } else {
+            System.out.println("The key is found at index " + index);
+        }
+
+        System.out.println("LinkList size = " + ll.size);
 
     }
 

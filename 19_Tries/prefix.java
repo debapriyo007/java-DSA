e23ecebd6b1f47f21ac0041346b4fe4c979cbeda
Debapriyo Das <debapriyo00@gmail.com>
2023-08-15 19:34:51 +0530

    The code is not completed yet.

---
+++ 19_Tries/prefix.java
@@ -0,0 +1,20 @@
+public class prefix {
+
+    static class Node{
+        Node[] children = new Node [26];
+        boolean eow = false;
+        int fre;
+        
+        public Node(){
+               for(int i = 0 ;i<children.length ;i++){
+                children[i] = null;
+                 
+               }
+               fre = 1; //for any new node frequency will be 1.
+        }
+    }
+    public static Node root = new Node();
+    public static void main(String[] args) {
+        String arr[] ={"zebra" ,"dog" , "duck" , "dove"};
+    }
+}
